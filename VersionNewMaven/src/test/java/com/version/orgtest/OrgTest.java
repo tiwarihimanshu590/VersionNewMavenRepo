22aca78836b2baede7cc3c2cb6447351c687f53d
HJELLO <HJELLO@DESKTOP-H572DK8>
2025-11-11 12:44:08 +0530

    added run time parameters for Org

--- VersionNewMaven/src/test/java/com/version/orgtest/OrgTest.java
+++ VersionNewMaven/src/test/java/com/version/orgtest/OrgTest.java
@@ -7,6 +7,15 @@ public class OrgTest {
 	@Test
 	public void createOrgTest()
 	{
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser","chrome");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
 		System.out.println("execute createOrgTest");
 	}
 	
