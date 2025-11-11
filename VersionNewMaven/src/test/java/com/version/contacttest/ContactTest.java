04d7b10282d99e3fb0da1f10593c254f6fc9dd3f
HJELLO <HJELLO@DESKTOP-H572DK8>
2025-11-11 12:14:05 +0530

    added deleteContactTest()

--- VersionNewMaven/src/test/java/com/version/contacttest/ContactTest.java
+++ VersionNewMaven/src/test/java/com/version/contacttest/ContactTest.java
@@ -24,5 +24,11 @@ public class ContactTest {
 	{
 		System.out.println("execute modifyContactTest");
 	}
+	
+	@Test
+	public void deleteContactTest()
+	{
+		System.out.println("execute deleteContactTest");
+	}
 
 }
