927f5f160e529614144937489ae713452e6089c8
HJELLO <HJELLO@DESKTOP-H572DK8>
2025-11-11 11:11:10 +0530

    first push of framework

---
+++ VersionNewMaven/src/test/java/com/version/orgtest/OrgTest.java
@@ -0,0 +1,19 @@
+package com.version.orgtest;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	
+	@Test
+	public void createOrgTest()
+	{
+		System.out.println("execute createOrgTest");
+	}
+	
+	@Test
+	public void modifyOrgTest()
+	{
+		System.out.println("execute modifyOrgTest");
+	}
+
+}
